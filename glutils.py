34ed96830ad252bba6cecef9899f326ecfdfc36d
andy <nguyen>
2019-11-23 10:38:29 -0800

    Remove SpeedRegulator, add documentation

--- glutils.py
+++ glutils.py
@@ -1,9 +1,30 @@
-import time
+"""
+A collection of utilities to manage OpenGL and ultimately
+abstract the hardware calls from the program
+"""
+
 import moderngl as gl
+
+
+def enable_depth_test(ctx):
+    """Enables the depth test flag"""
+    ctx.enable(gl.DEPTH_TEST)
+
+def enable_cull_face(ctx):
+    """Enables the cull face flag"""
+    ctx.enable(gl.CULL_FACE)
+
+def disable_depth_test(ctx):
+    """Disables the depth test flag"""
+    ctx.disable(gl.DEPTH_TEST)
+
+def disable_cull_face(ctx):
+    """Disables the cull face flag"""
+    ctx.disable(gl.DEPTH_TEST)
+
 
 class Model:
     """Manages a VAO of VBOs for rendering
-
     Assumes that data handed to it are numpy arrays"""
     
     def set_indices(self, indices):
@@ -11,6 +32,11 @@ class Model:
         self.indices_vbo.write(indices.tobytes())
 
     def __init__(self, ctx, indices=[], dynamic_draw=False):
+        """Initiates a model
+        indices are optional but may be provided upon creation
+        indices are assumed to contain 4-byte ints
+        dynamic_draw determines whether indices should be
+            buffered dynamically or not"""
         self.ctx = ctx
         self.vbos = list()
 
@@ -23,47 +49,22 @@ class Model:
 
 
     def add_vertex_data(self, location, data_type, data):
+        """Creates a VBO and stores the vertex pointers
+        data is assumed to be 4-byte floats
+        This is the GL equivalent to vertex attrib pointers"""
         vbo = self.ctx.buffer(data.tobytes())
         self.vbos.append( (vbo, data_type, location) )
         
     def create_vao(self, program):
+        """Creates the VAO for the model
+        Vertex data should be given before this is called"""
         self.vao = self.ctx.vertex_array(program, self.vbos, 
                 index_buffer=self.indices_vbo)
 
-    def draw(self, shape):
-        self.vao.render(shape)
-
     def draw_lines(self):
-        self.draw(gl.LINES)
+        """Draw lines"""
+        self.vao.render(gl.LINES)
         
     def draw_triangles(self):
-        self.draw(gl.TRIANGLES)
-
-
-class SpeedRegulator:
-            
-    def reset(self):
-        self.last_time = time.time()
-        self.last_time_fps = time.time()
-        self.fps_counter = 0
-        self.fps = 0
-            
-    def get_next_delta(self):
-        current_time = time.time()
-        speed = current_time - self.last_time
-        self.last_time = current_time
-        
-        self.fps_counter += 1
-        if (current_time - self.last_time_fps) > self.avg_time:
-            self.fps = self.fps_counter / (current_time - self.last_time_fps)
-            self.fps_counter = 0
-            self.last_time_fps = current_time
-        
-        return speed
-        
-    def get_fps(self):
-        return self.fps 
-        
-    def __init__(self, avg_time):
-        self.avg_time = avg_time
-        self.reset()
+        """Draw triangles"""
+        self.vao.render(gl.TRIANGLES)
